f95b5692a48520783fab9d77dd45eed8b4347e5b
David Malary <davidmalary@gmail.com>
2015-11-28 13:04:20 -0500

    server js buttons added for delete/edit. working on app.js

--- public/app.js
+++ public/app.js
@@ -123,7 +123,7 @@ $(function(){
       // loop through our data to make markers
       for (var i = 0; i < data.length; i++) {
 
-        var eventInfo = data[i].info + "<a href='http://google.com' class='button'>Add to Bucket List</a>"
+        var eventInfo = data[i].info + "<br>" + "<a href='http://google.com' class='button'>Add to Bucket List</a>"
         var marker = new google.maps.Marker ({
           position: data[i].location,
           map: map,
@@ -173,10 +173,59 @@ $(function(){
     });
   };
 
+  var editUser = function($id){
+    console.log($id)
+  };
+
+
+  console.log(user);
 }); // END OF WINDOW ONLOAD
 
 
 // TEMP STUFF && GARBAGE//////////////////////////////////////////////////
-
+// var editInstructor = function($id) {
+//   // start by finding the id of the instructor. it's in the instructor-container class!
+//   console.log($id);
+//   $.ajax({
+//     url: "http://localhost:3000/instructors/" + $id,
+//     method: "GET",
+//     // data: 'json'
+//   }).done(updateForm);
+// ​
+//   // code the ajax call, be sure to add the id onto the url
+//   // use updateForm for the callback to .done()
+// };
+// ​
+// var updateForm = function(data) {
+//   var resultDiv = $('#form-container');
+//   $("#horizontal").empty();
+//   resultDiv.empty();
+//   resultDiv.show();
+//   var template = Handlebars.compile($("#instructor-edit-template").html());
+//   resultDiv.append(template(data));
+//   $('.edit-instructor-submit').click(function () {
+//     saveUpdate();
+//   });
+// };
+// ​
+// var saveUpdate = function() {
+//   var $id = $('#instructor-id').val();
+//   // What other variables do  we need in the form?
+//   var $instructorName = $('#instructor-name').val();
+//   var $instructorMotto = $('#instructor-motto').val();
+// ​
+//   // create an instructorData object mapping the same k/v pairs to the variables you just made
+//   var instructorData = {
+//     name: $instructorName,
+//     motto: $instructorMotto
+//   }
+// ​
+//   // Make the ajax call similar to createInstructor except make it a put request
+//   $.ajax({
+//     url: "http://localhost:3000/instructors/" + test,
+//     method: "PUT",
+//     data: instructorData
+//   }).done(getInstructors);
+// };
 
 
